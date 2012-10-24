4e68ea3dca304d21daa81f55c9d80771e8a3a81c
Stewart Anderson <git@stewart-anderson.co.uk>
2012-10-24 23:31:20 +0100

    Starting re-write of select element

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -14,7 +14,8 @@
             matched = [];
 
         //  create the initial array of objects - containing all values and text 
-        //  of each child option of the select
+        //  of each child option of the select, we'll use this to scan for 
+        //  matches later
         this.children().each(function (index) {
             data[index] = {
                 value: $(this).attr('value'),
@@ -22,7 +23,13 @@
             }
         });
 
-        
+        //  re-write select element with two ipputs, one hidden and the other 
+        //  to be used to accept user inputs
+        $("<input/>", {
+            id: this.attr('id'),
+            name: this.attr('name'),
+            type: 'hidden'
+        }).appendTo(this.parent());
         return this;
     };
 }(jQuery));
