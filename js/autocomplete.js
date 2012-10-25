f6a46a94f17168b8c9fb785dfee95bd578716d4a
Stewart Anderson <git@stewart-anderson.co.uk>
2012-10-25 15:26:42 +0100

    Update to auttocomplete.js - adding key up events

--- js/autocomplete.js
+++ js/autocomplete.js
@@ -20,16 +20,50 @@
             data[index] = {
                 value: $(this).attr('value'),
                 text: $(this).text()
-            }
+            };
         });
 
         //  re-write select element with two ipputs, one hidden and the other 
         //  to be used to accept user inputs
+
         $("<input/>", {
             id: this.attr('id'),
             name: this.attr('name'),
             type: 'hidden'
         }).appendTo(this.parent());
-        return this;
+
+        $("<input/>", {
+            id: 'AC_' + this.attr('id'),
+            type: 'text'
+        }).appendTo(this.parent());
+
+        //  now bind any key events on the new input
+        $("#AC_" + this.attr('id')).keyup(function (event) {
+            var value = $(this).val();
+            switch (event.keyCode) {
+            case 9:     //  tab key
+                //  hide the ac results
+                break;
+            case 13:    //  enter key
+                //  hide the ac results
+                break;
+            case 38:    //  up arrow key
+                //  up a record
+                break;
+            case 40:    //  down arrow key
+                //  down a record
+                break;
+            //  if none of the targeted keys are hit, we're safe to assume the 
+            //  user is waiting for a list of matches to be returned
+            default:
+                //  strip out any funky chars
+                value = value.replace(/[^\w\s]/gi, '');
+                $(this).val(value);
+                //  call autocomplete function
+                break;
+            }
+        });
+        //  remove the first instance of the select now we have all the info
+        return this.remove();
     };
 }(jQuery));
